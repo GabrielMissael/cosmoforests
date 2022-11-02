e2bb71891d5b28370329f49bb21416c65b25dd8e
GabrielMissael <missaelgabo@gmail.com>
2022-11-02 16:25:27 -0600

    Create mask in new function

--- cosmoforests/mask_forest.py
+++ cosmoforests/mask_forest.py
@@ -29,22 +29,30 @@ def mask_forest(z, wv, fluxes, lambda_min = 1420.0, lambda_max = 1520.0):
     # Number of bins
     n_bins = len(wv)
 
-    # Get wavelength interval of CIV forest
+    new_fluxes = np.zeros((len(z), n_bins))
+    mask = get_mask(wv, z, lambda_min = lambda_min, lambda_max = lambda_max)
+
+    # Remove non-CIV-forest absorptions
+    for i, flux in enumerate(fluxes):
+        # Masks the CIV forest
+        new_fluxes[i] = flux
+        # Makes 1 whatever isn't in the CIV forest
+        new_fluxes[i][~mask[i]] = -2.0
+    
+        #! TODO: Do we need to normalize the fluxes?
+
+    return new_fluxes
+
+def get_mask(wv, z, lambda_min = 1420.0, lambda_max = 1520.0):
+    """Create a mask for a given wavelength range."""
+
     z_min = np.min(z)
     z_max = np.max(z)
     lambda_min_obs = lambda_min*(1.0 + z)
     lambda_max_obs = lambda_max*(1.0 + z)
 
-    new_fluxes = np.zeros((len(z), n_bins))
+    mask = []
+    for i in range(len(z)):
+        mask.append((wv > lambda_min_obs[i]) & (wv < lambda_max_obs[i]))
 
-    # Remove non-CIV-forest absorptions
-    for i, flux in enumerate(fluxes):
-        # Masks the CIV forest
-        mask = (wv > lambda_min_obs[i]) & (wv < lambda_max_obs[i])
-        new_fluxes[i] = flux
-        # Makes 1 whatever isn't in the CIV forest
-        new_fluxes[i][~mask] = -2.0
-    
-        #! TODO: Do we need to normalize the fluxes?
-
-    return new_fluxes
+    return mask
